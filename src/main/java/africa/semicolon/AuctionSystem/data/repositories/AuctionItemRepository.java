2ee42e29461f9bd9c6b3b9e97390a862e6016ba7
ajala ahmad <ahmadajala98@gmail.com>
2022-04-15 20:55:29 +0100

    Auction Item Repostory

--- src/main/java/africa/semicolon/AuctionSystem/data/repositories/AuctionItemRepository.java
+++ src/main/java/africa/semicolon/AuctionSystem/data/repositories/AuctionItemRepository.java
@@ -1,5 +1,6 @@
 package africa.semicolon.AuctionSystem.data.repositories;
 
+import africa.semicolon.AuctionSystem.data.models.AppUser;
 import africa.semicolon.AuctionSystem.data.models.AuctionItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,6 +9,7 @@ import java.util.Optional;
 
 public interface AuctionItemRepository extends JpaRepository<AuctionItem, Long> {
     Optional<List<AuctionItem>> findAuctionItemByItemName(String itemName);
-    Optional<List<AuctionItem>> findAuctionItemByAppUserId(Long id);
-
+    Optional<List<AuctionItem>> findAuctionItemBySeller(AppUser seller);
+    Optional<List<AuctionItem>> findAuctionItemByBasePrice(Double price);
+    Optional<List<AuctionItem>> findAuctionItemBySellerFirstNameAndSellerLastName(String sellerFirstName, String sellerLastName);
 }
