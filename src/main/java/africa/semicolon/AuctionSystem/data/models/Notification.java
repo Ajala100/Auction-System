9757755e30527a098550c6a5e8bf46fe8cfcc02f
ajala ahmad <ahmadajala98@gmail.com>
2022-04-15 20:51:49 +0100

    Notification model

--- src/main/java/africa/semicolon/AuctionSystem/data/models/Notification.java
+++ src/main/java/africa/semicolon/AuctionSystem/data/models/Notification.java
@@ -1,24 +1,32 @@
 package africa.semicolon.AuctionSystem.data.models;
 
-import lombok.Data;
+import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
-import java.util.List;
+import java.util.Set;
 
 @Entity
-@Data
+@Setter
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class Notification {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @Column(nullable = false)
+    @NotBlank(message = "Notification event cannot be null")
+    @Size(max=200, message = "Notification even cannot be more than 200 characters long")
     private String event;
 
-    @ManyToMany
-    private List<AppUser> appUser;
+    @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
+    private Set<AppUser> appUser;
 
     @CreationTimestamp
     private LocalDateTime timeCreated;
