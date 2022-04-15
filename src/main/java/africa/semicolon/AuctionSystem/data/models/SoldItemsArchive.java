ccae70ca46141a2cd44f3f01777dfbeaf8226b94
ajala ahmad <ahmadajala98@gmail.com>
2022-04-15 20:52:33 +0100

    Sold item archive model

--- src/main/java/africa/semicolon/AuctionSystem/data/models/SoldItemsArchive.java
+++ src/main/java/africa/semicolon/AuctionSystem/data/models/SoldItemsArchive.java
@@ -1,22 +1,29 @@
 package africa.semicolon.AuctionSystem.data.models;
 
+import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
 public class SoldItemsArchive {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @OneToOne
+    @OneToOne(fetch = FetchType.EAGER, cascade = CascadeType.PERSIST )
     private AuctionItem auctionItem;
 
-    @OneToOne
+    @OneToOne(fetch = FetchType.EAGER, cascade =CascadeType.PERSIST)
     private Bid bid;
 
     @CreationTimestamp
     private LocalDateTime timeSold;
+
 }
