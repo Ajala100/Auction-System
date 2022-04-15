b3f35d19121be961005b545601590f5b7cdfc7ac
ajala ahmad <ahmadajala98@gmail.com>
2022-04-15 20:50:17 +0100

    Auction item model

--- src/main/java/africa/semicolon/AuctionSystem/data/models/AuctionItem.java
+++ src/main/java/africa/semicolon/AuctionSystem/data/models/AuctionItem.java
@@ -1,29 +1,38 @@
 package africa.semicolon.AuctionSystem.data.models;
 
-import lombok.Data;
+import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 
 @Entity
-@Data
+@Setter
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class AuctionItem {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @NotBlank(message = "Auction item name cannot be null")
     private String itemName;
 
+    @NotBlank(message = "Auction Item Description cannot be blank.")
+    @Size(max = 200, message = "Item description must not be more than 200 characters long")
     private String description;
 
-    @Column(nullable = false)
+    @NotBlank(message = "Auction item base price cannot be null")
     private Double basePrice;
 
     private String itemImageUrl;
 
     @OneToOne(cascade = CascadeType.PERSIST, fetch = FetchType.EAGER)
-    private AppUser appUser;
+    private AppUser seller;
 
     @CreationTimestamp
     private LocalDateTime timeListed;
