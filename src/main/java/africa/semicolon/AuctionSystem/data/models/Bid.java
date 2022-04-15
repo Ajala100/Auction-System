8cde0e754aab74f02119378c503d89f20a74bf41
ajala ahmad <ahmadajala98@gmail.com>
2022-04-15 20:51:11 +0100

    Bid model

--- src/main/java/africa/semicolon/AuctionSystem/data/models/Bid.java
+++ src/main/java/africa/semicolon/AuctionSystem/data/models/Bid.java
@@ -1,26 +1,32 @@
 package africa.semicolon.AuctionSystem.data.models;
 
-import lombok.Data;
+import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 import java.time.LocalDateTime;
 
 @Entity
-@Data
+@Setter
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class Bid {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @Column(nullable = false)
+    @NotBlank(message = "Bid offer can not be blank")
     private Double offer;
 
-    @OneToOne
-    private AppUser appUser;
+    @OneToOne(fetch = FetchType.EAGER, cascade = CascadeType.PERSIST)
+    private AppUser bidder;
 
     @OneToOne
-    private AuctionItem item;
+    private AuctionItem auctionItem;
 
     @CreationTimestamp
     private LocalDateTime timeOfBid;
