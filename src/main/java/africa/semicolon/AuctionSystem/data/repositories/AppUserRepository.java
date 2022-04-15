c19f4e52d730a13c295bc6aff291b7f42d342bf3
ajala ahmad <ahmadajala98@gmail.com>
2022-04-15 20:53:48 +0100

    App user respository

--- src/main/java/africa/semicolon/AuctionSystem/data/repositories/AppUserRepository.java
+++ src/main/java/africa/semicolon/AuctionSystem/data/repositories/AppUserRepository.java
@@ -2,12 +2,10 @@ package africa.semicolon.AuctionSystem.data.repositories;
 
 import africa.semicolon.AuctionSystem.data.models.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface AppUserRepository extends JpaRepository<AppUser, Long> {
-
     Optional<AppUser> findAppUserByEmail(String email);
-
+    Optional<AppUser> findAppUserByPhoneNumber(String phoneNumber);
 }
