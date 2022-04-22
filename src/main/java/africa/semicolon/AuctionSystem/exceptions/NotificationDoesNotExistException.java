436f0f35807a2a4aa784375bc419528b3fce1a36
ajala ahmad <ahmadajala98@gmail.com>
2022-04-22 18:20:25 +0100

    Notification does not exist exception

---
+++ src/main/java/africa/semicolon/AuctionSystem/exceptions/NotificationDoesNotExistException.java
@@ -0,0 +1,7 @@
+package africa.semicolon.AuctionSystem.exceptions;
+
+public class NotificationDoesNotExistException extends Throwable{
+    public NotificationDoesNotExistException(String message){
+        super(message);
+    }
+}
