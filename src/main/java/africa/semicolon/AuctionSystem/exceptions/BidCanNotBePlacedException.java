872e643c77389582ca666d98600318aca00efd45
ajala ahmad <ahmadajala98@gmail.com>
2022-04-22 18:26:17 +0100

    Bid can not be placed exception

--- src/main/java/africa/semicolon/AuctionSystem/exceptions/BidCanNotBePlacedException.java
+++ src/main/java/africa/semicolon/AuctionSystem/exceptions/BidCanNotBePlacedException.java
@@ -1,2 +1,6 @@
-package africa.semicolon.AuctionSystem.exceptions;public class BidCanNotBePlacedException {
+package africa.semicolon.AuctionSystem.exceptions;
+
+public class BidCanNotBePlacedException extends Exception {
+    public BidCanNotBePlacedException(String message){
+    super(message);}
 }
