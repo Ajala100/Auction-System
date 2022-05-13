dda6a41f190fe44888447546e50cf119212ca6b8
ajala ahmad <ahmadajala98@gmail.com>
2022-05-13 17:49:07 +0100

    Bid repository

--- src/main/java/africa/semicolon/AuctionSystem/data/repositories/BidRepository.java
+++ src/main/java/africa/semicolon/AuctionSystem/data/repositories/BidRepository.java
@@ -1,5 +1,7 @@
 package africa.semicolon.AuctionSystem.data.repositories;
 
+import africa.semicolon.AuctionSystem.data.models.AppUser;
+import africa.semicolon.AuctionSystem.data.models.AuctionItem;
 import africa.semicolon.AuctionSystem.data.models.Bid;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -7,7 +9,11 @@ import java.util.List;
 import java.util.Optional;
 
 public interface BidRepository extends JpaRepository<Bid, Long> {
-    Optional<List<Bid>> findBidByAppUserId(Long id);
+    Optional<List<Bid>> findBidByBidderId(Long bidderId);
+    Optional<List<Bid>> findBidByBidder(AppUser bidder);
     Optional<List<Bid>> findBidByOffer(Double offer);
+    Optional<List<Bid>> findBidByAuctionItem(AuctionItem item);
+    Optional<List<Bid>> findBidByAuctionItemId(Long itemId);
+    Optional<Bid> findBidByAuctionItemAndOffer(AuctionItem item, Double offer);
 
 }
